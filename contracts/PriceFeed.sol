c4f9d39992dfb845fc74c9889473d671b3b41237
bun919tw <bun919tw@gmail.com>
2021-08-13 18:46:55 +0800

    contracts/: support ETH and USD quote

--- contracts/PriceFeed.sol
+++ contracts/PriceFeed.sol
@@ -2,25 +2,68 @@
 
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol";
+import "@chainlink/contracts/src/v0.8/Denominations.sol";
 import "./interfaces/IPriceFeed.sol";
 
 contract PriceFeed is IPriceFeed {
-    AggregatorV3Interface public priceFeed;
+    address public registry;
+    address public token;
+    address public base;
+    address public quote;
 
-    constructor(address _priceFeed) {
-        priceFeed = AggregatorV3Interface(_priceFeed);
+    /**
+     * @dev Sets the values for {registry}, {token}, {base}, and {quote}.
+     *
+     * We retrieve price from ChainLink registry, and normally {token} and {base} should be the same.
+     * Also, we currently only support ETH and USD as quote.
+     */
+    constructor(address _registry, address _token, address _base, address _quote) {
+        // We only support ETH and USD quote.
+        require(_quote == Denominations.ETH || _quote == Denominations.USD, "unsupport quote");
+
+        // Make sure the aggregator exists.
+        AggregatorV2V3Interface aggregator = FeedRegistryInterface(_registry).getFeed(_base, _quote);
+        require(FeedRegistryInterface(_registry).isFeedEnabled(address(aggregator)), "aggregator not enabled");
+
+        registry = _registry;
+        token = _token;
+        base = _base;
+        quote = _quote;
     }
 
     /**
-     * @notice Return the latest price.
+     * @notice Return the token. It should be the collateral token address from IB agreement.
+     * @return the token address
+     */
+    function getToken() external override view returns (address) {
+        return token;
+    }
+
+    /**
+     * @notice Return the token latest price in USD.
      * @return the price, scaled by 1e18
      */
     function getPrice() external override view returns (uint) {
-        ( , int price, , , ) = priceFeed.latestRoundData();
+        uint price = getPriceFromChainlink(base, quote);
+        if (quote == Denominations.ETH) {
+            uint ethUsdPrice = getPriceFromChainlink(Denominations.ETH, Denominations.USD);
+            price = price * ethUsdPrice / 1e18;
+        }
+        return price;
+    }
+
+    /**
+     * @notice Get the token price from ChainLink.
+     * @param _base The base
+     * @param _quote The quote
+     * @return the price, scaled by 1e18
+     */
+    function getPriceFromChainlink(address _base, address _quote) internal view returns (uint) {
+        ( , int price, , , ) = FeedRegistryInterface(registry).latestRoundData(_base, _quote);
         require(price > 0, "invalid price");
 
         // Extend the decimals to 1e18.
-        return uint(price) * 10**(18 - uint(priceFeed.decimals()));
+        return uint(price) * 10**(18 - uint(FeedRegistryInterface(registry).decimals(_base, _quote)));
     }
 }
