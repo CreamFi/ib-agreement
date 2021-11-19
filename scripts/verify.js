9da395ee26f6505fe633e8d0306683204b970932
Jeremy Yang <jeremy@cream.finance>
2021-11-19 20:15:59 +0800

    Modify verify script for v2

--- scripts/verify.js
+++ scripts/verify.js
@@ -1,21 +1,34 @@
-const { run } = require("hardhat");
+const { ethers, run } = require("hardhat");
 
+// async function priceFeed() {
+//   const feedAddress = '';
 
+//   const registry = '0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf';
+//   const token = '0x2ba592F78dB6436527729929AAf6c908497cB200';
+//   const quote = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+
+//   await run('verify:verify', {
+//     address: feedAddress
+//   });
+// }
 
 async function main() {
-  const ibAgreementAddress = "0x30f254104A8a7B7779483EEEf5560397A737f812";
+  const ibAgreementAddress = "0x9ae50BD64e45fd87dD05c768ff314b8FE246B3fF";
 
   const executor = "0x6d5a7597896a703fe8c85775b23395a48f971305";
-  const borrower = "0x6Af3D183d225725d975C5EaA08D442dd01Aad8fF";
-  const governor = "0x545c18f845d6b37Ea06Bbc83bd9F8A54f4AAb075";
-  const cy = "0xBE86e8918DFc7d3Cb10d295fc220F941A1470C5c";
-  const collateral = "0x2ba592F78dB6436527729929AAf6c908497cB200";
+  const borrower = "0x431e81E5dfB5A24541b5Ff8762bDEF3f32F96354";
+  const governor = "0x037fb55cfA48994Ed020eD5Be63c2770FB725e7B";
+  const comptroller = "0xAB1c342C7bf5Ec5F02ADEA1c2270670bCa144CbB";
+  const collateral = "0x4E15361FD6b4BB609Fa63C81A2be19d873717870";
+  const priceFeed = "0xBfC408229184c3e800c065445295878f915599a9";
+  const collateralFactor = ethers.utils.parseEther('0.75');
+  const liquidationFactor = ethers.utils.parseEther('0.85');
 
   await run("verify:verify", {
     address: ibAgreementAddress,
-    contract: "contracts/IBAgreement.sol:IBAgreement",
+    contract: "contracts/IBAgreement.sol:IBAgreementV2",
     constructorArguments: [
-      executor, borrower, governor, cy, collateral
+      executor, borrower, governor, comptroller, collateral, priceFeed, collateralFactor, liquidationFactor
     ]
   });
 }
