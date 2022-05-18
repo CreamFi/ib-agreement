7ba2aebe1f1574448d1640914d4e809518465048
loodaobao <warren.y.cheng@gmail.com>
2022-05-18 10:25:28 +0800

    remove console.log

--- contracts/EasierConverter.sol
+++ contracts/EasierConverter.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "hardhat/console.sol";
 
 interface IUniswapV2Router {
     function swapExactTokensForTokens(
@@ -49,21 +48,17 @@ contract EasyConverter {
      */
     function convert(uint256 amount) external {
         require(IERC20(sourceToken).balanceOf(address(this)) >= amount, "amount not enough");
-        console.log("before approve");
         IERC20(sourceToken).safeApprove(uniswapV2Router, amount);
-        console.log("after approve");
         address[] memory paths;
         paths = new address[](3);
         paths[0] = sourceToken;
         paths[1] = wrappedNativeAddress;
         paths[2] = destinationToken;
-        console.log("before swap");
         IUniswapV2Router(uniswapV2Router).swapExactTokensForTokens(
                     amount,
                     0,
                     paths,
                     msg.sender,
                     block.timestamp);
-        console.log("after swap");
     }
 }
